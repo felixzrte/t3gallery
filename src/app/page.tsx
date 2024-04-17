94be396e4a88a30bc7f2cad0c13afa6e1a2b401f
Felix <felixzarate14@gmail.com>
2024-04-17 10:32:46 -0400

    link db to UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,21 @@
-import { index } from "drizzle-orm/mysql-core";
-import { url } from "inspector";
-
+import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
-export default function HomePage() {
-  const mockUrls = [
-    "https://utfs.io/f/88487f8e-fc86-4513-b0a1-4c1cdfe8be3c-ttbe2f.webp",
-    "https://utfs.io/f/a599fa96-a2d0-4137-a0da-74b872644b7b-snnk5j.webp",
-    "https://utfs.io/f/000c9442-17df-4270-b7ca-79f60f67fd14-7rywl7.webp",
-    "https://utfs.io/f/88487f8e-fc86-4513-b0a1-4c1cdfe8be3c-ttbe2f.webp",
-  ];
-
-  const mockImages = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url,
-  }));
+export default async function HomePage() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="px-4">
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
+        {images.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="image" />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
-      Hello gallery in progress
     </main>
   );
 }
